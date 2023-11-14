9a221108a93a42f6a61c5422eefc234bbb6d5888
Maxime CAT <maximecat@outlook.fr>
2023-11-14 09:15:21 +0100

    HomePage App (test react)

--- todolist-react/src/pages/App.jsx
+++ todolist-react/src/pages/App.jsx
@@ -1,12 +1,22 @@
 import './App.css';
+import Header from '../components/Header/Header';
+import ApiService from '../utils/services/ApiService'
+import FormAddTodo from '../components/FormAddTodo/FormAddTodo';
+import TodoCard from '../components/TodoCard/TodoCard';
 
 function App() {
 
+  const datasService = new ApiService()
+
   return (
     <div className="App">
-      <header>
-        <h1>Hello</h1>
-      </header>
+      <Header />
+      <main id='main'>
+        <FormAddTodo />
+        <TodoCard task={'premier tâche'} />
+        <TodoCard task={'deuxième tâche'} />
+        <TodoCard task={'troisième tâche'} />
+      </main>
     </div>
   );
 }
