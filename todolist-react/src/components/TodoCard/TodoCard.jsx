8b1efa8f8f7ef26c6c9adab1055fee830b278b8f
Maxime CAT <maximecat@outlook.fr>
2023-11-21 14:39:52 +0100

    TodoCard componant

--- todolist-react/src/components/TodoCard/TodoCard.jsx
+++ todolist-react/src/components/TodoCard/TodoCard.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from 'react'
+import ApiService from '../../utils/services/ApiService'
 import './TodoCard.css'
 
-function TodoCard({ task }) {
+function TodoCard({ task, id, checked }) {
+    const [completed, setCompleted] = useState(checked)
+    const datasService = new ApiService()
+
+    const deleteTodo = () => {
+        console.log(id);
+        datasService.deleteTodoElement(id)
+            .then(todo => console.log(todo))
+    }
+
+    const updateTodo = () => {
+        // if (checked === true) {
+        //     setCompleted(false)
+        // } else {
+        //     setCompleted(true)
+        // }
+        setCompleted(!completed)
+    }
+
+    useEffect(() => {
+        datasService.updateTodoElement(id, completed)
+            .then(todo => console.log(todo))
+    }, [id, datasService, completed])
+
     return (
-        <article className='todo-card'>
-            <input type="checkbox" className='todo-checkbox' />
+        <article id={id} className='todo-card'>
+            <input type="checkbox" className='todo-checkbox' checked={completed} onClick={updateTodo} />
             <p className='todo-paragraph'>{task}</p>
-            <button className='todo-remove'>Remove</button>
+            <button className='todo-remove' onClick={deleteTodo}>Remove</button>
         </article>
     )
 }
