81b97d092ad0a6bc18e6747a3752e4bf98640c26
Maxime CAT <maximecat@outlook.fr>
2023-11-21 14:40:14 +0100

    HomePage

--- todolist-react/src/pages/App.jsx
+++ todolist-react/src/pages/App.jsx
@@ -1,21 +1,31 @@
 import './App.css';
 import Header from '../components/Header/Header';
-import ApiService from '../utils/services/ApiService'
 import FormAddTodo from '../components/FormAddTodo/FormAddTodo';
 import TodoCard from '../components/TodoCard/TodoCard';
+import ApiService from '../utils/services/ApiService'
+import { useEffect, useState } from 'react';
 
 function App() {
-
+  //
+  const [todos, setTodos] = useState([]);
   const datasService = new ApiService()
+
+  useEffect(() => {
+    datasService.getTodoList()
+      .then(todosRes => {
+        console.log(todosRes);
+        setTodos(todosRes)
+      })
+  }, [])
 
   return (
     <div className="App">
       <Header />
       <main id='main'>
         <FormAddTodo />
-        <TodoCard task={'premier tâche'} />
-        <TodoCard task={'deuxième tâche'} />
-        <TodoCard task={'troisième tâche'} />
+        {
+          todos.map(todo => <TodoCard task={todo.todo} id={todo.id} checked={todo.completed} />)
+        }
       </main>
     </div>
   );
